236279c973fb0311f0f53be9e2fa2780d4ed9123
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-05-03 09:39:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,5 +5,16 @@
 
 //Define the function 
 function showMessage(fName,lName, school) {
-    
+    //Body of function
+    const myOutput = 'My name is ${fName} ${lName} and I as a student at ${school}.';
+    console.log(myOutput);
 }
+
+// Prompt user for three pieces of information
+const firstName = prompt('Please enter your first name\n');
+const lastName = prompt('Please enter your last name\n');
+const homeschool = prompt('Please enter your last name\n');
+
+// Call the function and pass (send)
+// three pieces of info up to the showMessage ( ) function
+showMessage(firstName, lastName, homeschool);
